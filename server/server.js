ecf9a1612f50744d018b5dd8e30cac3b6be60568
asiasAtWork <asiasatwork@gmail.com>
2023-08-11 02:25:03 -0500

    Connect database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const goalRoutes = require('./routes/goals');
 
 // Create express app
@@ -17,9 +18,16 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/goals', goalRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-})
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to database and listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
